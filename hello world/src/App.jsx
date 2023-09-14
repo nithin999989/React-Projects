3ffeda195aac56acfe30e1df522eb973790b44f8
20481A05F2 <20481A05F2@gecgudlvallerumic.in>
2023-09-14 11:51:50 +0530

    commited

---
+++ hello world/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='HelloWorld'>
+      <h1>Hello World</h1>
+     
+      </div>
+      
+    </>
+  )
+}
+
+export default App
